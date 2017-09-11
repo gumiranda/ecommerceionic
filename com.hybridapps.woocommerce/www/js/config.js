18507b3d62fdc8ea036c979ea8bdadce720a5df8
root <root@gumiranda.gumiranda>
2017-09-11 06:02:55 -0300

    commitbrabo

--- com.hybridapps.woocommerce/www/js/config.js
+++ com.hybridapps.woocommerce/www/js/config.js
@@ -1,11 +1,11 @@
 angular.module("woocommerce-api").constant("CONFIG", {
 
     // The url of your domain, both HTTP and HTTPS are supported.
-    site_url: 'http://mobile-apps.today/products/ionic/woocommerce-api',
+    site_url: 'http://www.heliexpress.com.br/app/products/ionic/woocommerce-api',
     // site_url: 'http://localhost:8888/mobile/woocommerce-api/wordpress',
 
     // Max period of time to wait for reply from the server, defined in milliseconds.
-    request_timeout: 6000,
+    request_timeout: 5000,
 
     // The url that follows your main domain, the API version is of interest here, v3 is the latest.
     wc_api_endpoint: '/wc-api/v3',
@@ -16,8 +16,8 @@ angular.module("woocommerce-api").constant("CONFIG", {
 
     // Pair of credentials from your woocommerce installation, please refer to the documentation.
     // Apps today
-    wc_consumer_key: 'ck_5f886e3d5c14cb0280167e5c96615e59a07b2124',
-    wc_consumer_secret: 'cs_4a5d2c133acc4203d45b410cf830f2755bf3257e',
+    wc_consumer_key: 'ck_ce7b0c400de77e866e24d9a191445877b0dba97f',
+    wc_consumer_secret: 'cs_a7546ec6dc2b794d0a3fdaf390328523b7756e4c',
     // Localhost
     // wc_consumer_key: 'ck_86a13f2c0ec34a4c99374e120dba436d31f069c3',
     // wc_consumer_secret: 'cs_a0bc69e05eced4a8f464c08849c8f543946448d9',
